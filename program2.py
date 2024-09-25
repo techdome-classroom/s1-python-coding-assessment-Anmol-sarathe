94b740bbfe3e7fa4e012e4161c4a5cd93934c1be
Anmol-sarathe <0112cs211022@gmail.com>
2024-09-25 13:46:02 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -1,16 +1,13 @@
 def matches(secret, pattern):
     m, n = len(secret), len(pattern)
     dp = [[False] * (n + 1) for _ in range(m + 1)]
-    
-    # Base case
+  
     dp[0][0] = True
-    
-    # Handle patterns like , *, * etc. at the start
+   
     for j in range(1, n + 1):
         if pattern[j - 1] == '*':
             dp[0][j] = dp[0][j - 1]
-
-    # Fill the DP tab
+    
     for i in range(1, m + 1):
         for j in range(1, n + 1):
             if pattern[j - 1] == secret[i - 1] or pattern[j - 1] == '?':
